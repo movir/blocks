cc17793da7568ba613fbef2bc0d930bdc8001eed
Igor Molyarov <igor.molyarov@revelsystems.com>
2018-07-10 12:50:25 +0300

    upd

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,76 +1,90 @@
-import React, { PureComponent } from "react";
-import styled from "styled-components";
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
 
-const LinkItem = styled("div")`
-  position: absolute;
-  height: 1px;
-  transform-origin: 0;
-  background: black;
-  top: ${({ top }) => top || 0}px;
-  left: ${({ left }) => left || 0}px;
-  width: ${({ width }) => width || 0}px;
-  transform: rotate(${({ rotate }) => rotate || 0}rad);
-  z-index: 5;
+const LinkItem = styled('div')`
+    position: absolute;
+    height: 1px;
+    transform-origin: 0;
+    background: black;
+    top: ${({ top }) => top || 0}px;
+    left: ${({ left }) => left || 0}px;
+    width: ${({ width }) => width || 0}px;
+    transform: rotate(${({ rotate }) => rotate || 0}rad);
+    z-index: 5;
+`;
+
+const DelButton = styled('div')`
+    width: 10px;
+    margin: 0 auto;
+    position: relative;
+    top: -0.5em;
+    cursor: pointer;
 `;
 
 class Link extends PureComponent {
-  render() {
-    const { start, end, linkItem } = this.props;
-
-    const line = this.calcLine(this.getCenter(start), this.getCenter(end));
-
-    const lineParams = {
-      top: line.y1,
-      left: line.x1,
-      width: line.width,
-      rotate: line.angle
-    };
-
-    return (
-      <div
-        style={{
-          position: "absolute",
-          height: "1px",
-          transformOrigin: 0,
-          background: "black",
-          top: `${lineParams.top || 0}px`,
-          left: `${lineParams.left || 0}px`,
-          width: `${lineParams.width || 0}px`,
-          transform: `rotate(${lineParams.rotate || 0}rad)`,
-          zIndex: 5
-        }}
-        id={linkItem.id}
-        {...lineParams}
-      />
-    );
-  }
-  getCenter = ({ top, left, width, height }) => [
-    +left + width / 2,
-    +top + height / 2
-  ];
-
-  calcLine = ([x1, y1], [x2, y2]) => {
-    const x = Math.abs(x2 - x1);
-    const y = Math.abs(y2 - y1);
-    const width = Math.sqrt(x ** 2 + y ** 2);
-    let angle = Math.acos(x / width);
-    if (y2 < y1) {
-      angle = -angle;
+    constructor(props) {
+        super(props);
+        this.removeLink = this.removeLink.bind(this);
     }
 
-    if (x2 < x1) {
-      angle = Math.PI - angle;
+    removeLink(linkId) {
+        return () => this.props.rmLink(linkId);
     }
+    render() {
+        const { start, end, linkItem } = this.props;
 
-    return {
-      x1,
-      y1,
-      x2,
-      y2,
-      width,
-      angle
+        const line = this.calcLine(this.getCenter(start), this.getCenter(end));
+
+        const lineParams = {
+            top: line.y1,
+            left: line.x1,
+            width: line.width,
+            rotate: line.angle
+        };
+
+        return (
+            <div
+                id={linkItem.id}
+                style={{
+                    position: 'absolute',
+                    height: '1px',
+                    transformOrigin: 0,
+                    background: 'black',
+                    top: `${lineParams.top || 0}px`,
+                    left: `${lineParams.left || 0}px`,
+                    width: `${lineParams.width || 0}px`,
+                    transform: `rotate(${lineParams.rotate || 0}rad)`,
+                    zIndex: 5
+                }}
+            >
+                <DelButton onClick={this.removeLink(linkItem.id)}>X</DelButton>
+            </div>
+        );
+    }
+    getCenter = ({ top, left, width, height }) => [+left + width / 2, +top + height / 2];
+
+    calcLine = ([x1, y1], [x2, y2]) => {
+        const x = Math.abs(x2 - x1);
+        const y = Math.abs(y2 - y1);
+        const width = Math.sqrt(x ** 2 + y ** 2);
+        let angle = Math.acos(x / width);
+        if (y2 < y1) {
+            angle = -angle;
+        }
+
+        if (x2 < x1) {
+            angle = Math.PI - angle;
+        }
+
+        return {
+            x1,
+            y1,
+            x2,
+            y2,
+            width,
+            angle
+        };
     };
-  };
 }
 Link.propTypes = {};
 
--- src/components/Plate.js
+++ src/components/Plate.js
@@ -1,169 +1,166 @@
-import React, { Component } from "react";
-import { findDOMNode } from "react-dom";
-import PropTypes from "prop-types";
-import styled from "styled-components";
-import DragDropPlace from "./drag-n-drop";
-import { DragSource, DropTarget } from "react-dnd";
+import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import DragDropPlace from './drag-n-drop';
+import { DragSource, DropTarget } from 'react-dnd';
 
-import BlockItem from "./Block";
-import LinkItem from "./Link";
+import BlockItem from './Block';
+import LinkItem from './Link';
 
-import { BLOCK_TYPE } from "./constants";
+import { BLOCK_TYPE } from './constants';
 
-const PlateArea = styled("div")`
-  width: 80%;
-  height: 500px;
-  position: relative;
-  border: 1px solid;
-  margin: 15px auto;
+const PlateArea = styled('div')`
+    width: 80%;
+    height: 500px;
+    position: relative;
+    border: 1px solid;
+    margin: 15px auto;
 `;
 
 class Plate extends Component {
-  constructor() {
-    super();
-    this.state = {
-      blockIds: ["block1", "block2"],
-      blocks: {
-        block1: { id: "block1", top: 30, left: 30, width: 40, height: 35 },
-        block2: { id: "block2", top: 150, left: 460, width: 40, height: 35 }
-      },
-      linkIds: [],
-      links: {},
-      linking: null
-    };
+    constructor() {
+        super();
+        this.state = {
+            blockIds: ['block1', 'block2'],
+            blocks: {
+                block1: { id: 'block1', top: 30, left: 30, width: 40, height: 35 },
+                block2: { id: 'block2', top: 150, left: 460, width: 40, height: 35 }
+            },
+            linkIds: [],
+            links: {},
+            linking: null
+        };
 
-    this.linking = null;
+        this.linking = null;
 
-    this.linkIt = this.linkIt.bind(this);
-    this.startLinking = this.startLinking.bind(this);
-    this.endLinking = this.endLinking.bind(this);
-    this.updateBlockPosition = this.updateBlockPosition.bind(this);
-  }
-  linkIt(blockId) {
-    return () => {
-      if (this.linking === null) {
-        this.startLinking(blockId);
-      } else {
-        this.endLinking(blockId);
-      }
-    };
-  }
-  clearLinking(event) {
-    event && event.keyCode === 27 && console.log("\t", "clear by Esc");
-
-    if (!event || event.keyCode === 27) {
-      this.linking = null;
-      document.removeEventListener("keydown", this.clearLinking, false);
+        this.linkIt = this.linkIt.bind(this);
+        this.startLinking = this.startLinking.bind(this);
+        this.endLinking = this.endLinking.bind(this);
+        this.updateBlockPosition = this.updateBlockPosition.bind(this);
+        this.rmLink = this.rmLink.bind(this);
     }
-  }
-  startLinking(blockId) {
-    document.addEventListener("keydown", this.clearLinking, false);
-    this.linking = {
-      start: { id: blockId }
-    };
-  }
-  endLinking(blockId) {
-    this.linking["end"] = { id: blockId };
+    linkIt(blockId) {
+        return () => {
+            if (this.linking === null) {
+                this.startLinking(blockId);
+            } else {
+                this.endLinking(blockId);
+            }
+        };
+    }
+    clearLinking(event) {
+        event && event.keyCode === 27 && console.log('\t', 'clear by Esc');
 
-    const {
-      start: { id: startId },
-      end: { id: endId }
-    } = this.linking;
-    const { linkIds } = this.state;
+        if (!event || event.keyCode === 27) {
+            this.linking = null;
+            document.removeEventListener('keydown', this.clearLinking, false);
+        }
+    }
+    startLinking(blockId) {
+        document.addEventListener('keydown', this.clearLinking, false);
+        this.linking = {
+            start: { id: blockId }
+        };
+    }
+    endLinking(blockId) {
+        this.linking['end'] = { id: blockId };
 
-    //prevent self Linking
-    if (endId === startId) return this.clearLinking();
+        const { start: { id: startId }, end: { id: endId } } = this.linking;
+        const { linkIds } = this.state;
 
-    //prevent duplicating
-    if (linkIds.find(linkId => linkId === `${startId}-${endId}`))
-      return this.clearLinking();
+        //prevent self Linking
+        if (endId === startId) return this.clearLinking();
 
-    this.addLink({
-      id: `${startId}-${endId}`,
-      startId,
-      endId
-    });
-    this.clearLinking();
-  }
-  render() {
-    const { blockIds, blocks, linkIds, links } = this.state;
-    return (
-      <DragDropPlace>
-        <PlateArea>
-          {blockIds.map(blockId => (
-            <BlockItem
-              {...blocks[blockId]}
-              key={blockId}
-              onClick={this.linkIt(blockId)}
-            />
-          ))}
-          {linkIds.map(linkId => (
-            <LinkItem
-              key={linkId}
-              linkItem={links[linkId]}
-              start={blocks[links[linkId].startId]}
-              end={blocks[links[linkId].endId]}
-            />
-          ))}
-          <ConnectedDropField updateBlockPosition={this.updateBlockPosition} />
-        </PlateArea>
-      </DragDropPlace>
-    );
-  }
-  updateBlockPosition(id, positionParams) {
-    const blocks = this.state.blocks;
-    const block = { ...blocks[id], ...positionParams };
+        //prevent duplicating
+        if (linkIds.find(linkId => linkId === `${startId}-${endId}`)) return this.clearLinking();
 
-    this.setState({ blocks: { ...blocks, [id]: block } });
-  }
-  addLink(newLink) {
-    this.setState({
-      linkIds: [...this.state.linkIds, newLink.id],
-      links: { ...this.state.links, [newLink.id]: newLink }
-    });
-  }
+        this.addLink({
+            id: `${startId}-${endId}`,
+            startId,
+            endId
+        });
+        this.clearLinking();
+    }
+    render() {
+        const { blockIds, blocks, linkIds, links } = this.state;
+        return (
+            <DragDropPlace>
+                <PlateArea>
+                    {blockIds.map(blockId => (
+                        <BlockItem {...blocks[blockId]} key={blockId} onClick={this.linkIt(blockId)} />
+                    ))}
+                    {linkIds.map(linkId => (
+                        <LinkItem
+                            key={linkId}
+                            linkItem={links[linkId]}
+                            start={blocks[links[linkId].startId]}
+                            end={blocks[links[linkId].endId]}
+                            rmLink={this.rmLink}
+                        />
+                    ))}
+                    <ConnectedDropField updateBlockPosition={this.updateBlockPosition} />
+                </PlateArea>
+            </DragDropPlace>
+        );
+    }
+    updateBlockPosition(id, positionParams) {
+        const blocks = this.state.blocks;
+        const block = { ...blocks[id], ...positionParams };
+
+        this.setState({ blocks: { ...blocks, [id]: block } });
+    }
+    addLink(newLink) {
+        this.setState({
+            linkIds: [...this.state.linkIds, newLink.id],
+            links: { ...this.state.links, [newLink.id]: newLink }
+        });
+    }
+    rmLink(linkId) {
+        const links = { ...this.state.links };
+        delete links[linkId];
+
+        this.setState({
+            linkIds: this.state.linkIds.filter(_linkId => _linkId !== linkId),
+            links
+        });
+    }
 }
 
 const plateTarget = {
-  hover(props, monitor, component) {
-    const draggedItem = monitor.getItem();
-    const { x: dX = 0, y: dY = 0 } =
-      monitor.getDifferenceFromInitialOffset() || {};
-    props.updateBlockPosition(draggedItem.id, {
-      top: draggedItem.top + dY,
-      left: draggedItem.left + dX
-    });
-  },
-  drop(props, monitor, component) {
-    const draggedItem = monitor.getItem();
-    const { x: dX = 0, y: dY = 0 } =
-      monitor.getDifferenceFromInitialOffset() || {};
-    props.updateBlockPosition(draggedItem.id, {
-      top: draggedItem.top + dY,
-      left: draggedItem.left + dX
-    });
-  }
+    hover(props, monitor, component) {
+        const draggedItem = monitor.getItem();
+        const { x: dX = 0, y: dY = 0 } = monitor.getDifferenceFromInitialOffset() || {};
+        props.updateBlockPosition(draggedItem.id, {
+            top: draggedItem.top + dY,
+            left: draggedItem.left + dX
+        });
+    },
+    drop(props, monitor, component) {
+        const draggedItem = monitor.getItem();
+        const { x: dX = 0, y: dY = 0 } = monitor.getDifferenceFromInitialOffset() || {};
+        props.updateBlockPosition(draggedItem.id, {
+            top: draggedItem.top + dY,
+            left: draggedItem.left + dX
+        });
+    }
 };
 export default Plate;
 
 const DropField = ({ connectDropTarget, ...props }) =>
-  connectDropTarget(
-    <div
-      style={{
-        position: "absolute",
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0
-      }}
-    />
-  );
-const ConnectedDropField = DropTarget(
-  BLOCK_TYPE,
-  plateTarget,
-  (connect, monitor) => ({
+    connectDropTarget(
+        <div
+            style={{
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0
+            }}
+        />
+    );
+const ConnectedDropField = DropTarget(BLOCK_TYPE, plateTarget, (connect, monitor) => ({
     connectDropTarget: connect.dropTarget(),
     canDrop: monitor.canDrop(),
     isOver: monitor.isOver()
-  })
-)(DropField);
+}))(DropField);
